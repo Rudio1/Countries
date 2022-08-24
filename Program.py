833b6a4d69e35a5b5434891a38c47e924c241869
Guilherme Rudio <guilhermerudio6@gmail.com>
2022-08-24 14:49:36 -0300

    Add Formataçao do XLSX

--- Program.py
+++ Program.py
@@ -1,39 +1,43 @@
+from asyncio.windows_events import NULL
+from itertools import count
 import requests
 import json
 import xlsxwriter 
 
-
+#Criando o Excel e formatando.
 workbook = xlsxwriter.Workbook('Countries.xlsx') 
 worksheet = workbook.add_worksheet() 
+worksheet.write('A1', 'COUNTRIES LIST')
+worksheet.write('A2', 'Name') 
+worksheet.write('B2', 'Capital') 
+worksheet.write('C2', 'Area') 
+worksheet.write('D2', 'Currencies') 
 
-workbook.close() 
-
-
-istCountries = []
+listCountries = []
 listCapital = []
 listCurrencies = []
 listArea = []  
 
-
-
 url = requests.get("https://restcountries.com/v2/all")
 request = json.loads(url.content)
 formatJson = json.dumps(request, sort_keys=True, indent=4)
+count = 3
 for pais in request:
     try:
         listCountries.append(pais["name"])
         listCapital.append(pais["capital"])
         listCurrencies.append(pais["currencies"])
         listArea.append(pais["area"])
-        worksheet.write('A1', 'Hello..') 
-        worksheet.write('B1', 'Geeks') 
-        worksheet.write('C1', 'For') 
-        worksheet.write('D1', 'Geeks') 
     except(Exception):
+        print(listCapital)
         continue
-for info in listCurrencies:
-   code = info[0]["code"]
-
+    worksheet.write('A'+str(count), pais["name"])
+    worksheet.write('B'+str(count), pais["capital"])
+    worksheet.write('C'+str(count), pais["area"])
+    for info in listCurrencies:
+        worksheet.write('D'+ str(count), info[0]["code"])
+    count = count + 1
+workbook.close() 
 
     
 
